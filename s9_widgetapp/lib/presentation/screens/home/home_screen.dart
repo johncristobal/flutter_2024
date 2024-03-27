3068f4270348ea9701fed5b28ee66208168ed532
John Cristobal <johnalexiscristobaljimenez6@gmail.com>
2024-03-27 11:58:55 -0600

    HomeScreen set

--- s9_widgetapp/lib/presentation/screens/home/home_screen.dart
+++ s9_widgetapp/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:s9_widgetapp/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,8 +8,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
-      ),
+        title: const Text("Home"),
+       ),
+       body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItem.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItem[index];
+        return _CustomTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomTile extends StatelessWidget {
+  const _CustomTile({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(item.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward, color: colors.primary,),
+      title: Text(item.title),
+      subtitle: Text(item.subTitle),
+      onTap: () {
+        
+      },
     );
   }
 }
